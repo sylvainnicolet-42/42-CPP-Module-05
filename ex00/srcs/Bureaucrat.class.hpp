9bc579e2efffb291451d6ebdb2d012e40f8b8b82
Sylvain Nicolet <nicoletsylvain@gmail.com>
2023-08-12 14:57:24 +0200

    Adding ex00

---
+++ ex00/srcs/Bureaucrat.class.hpp
@@ -0,0 +1,47 @@
+
+#ifndef BUREAUCRAT_CLASS_HPP
+# define BUREAUCRAT_CLASS_HPP
+
+# include <iostream>
+
+class Bureaucrat {
+	public:
+		Bureaucrat();									// Default constructor
+		Bureaucrat(const Bureaucrat &src);				// Copy constructor
+		~Bureaucrat();									// Destructor
+		Bureaucrat &operator=(const Bureaucrat &rhs);	// Assignment operator
+
+		// Custom constructor
+		Bureaucrat(std::string const name, int grade);
+
+		// Getters and setters
+		std::string const	getName() const;
+		int					getGrade() const;
+
+		// Functions
+		void				incrementGrade();
+		void				decrementGrade();
+
+	private:
+		std::string const	_name;
+		int					_grade;
+
+	// Exceptions
+	class GradeTooHighException: public std::exception {
+	public:
+		virtual const char *what() const throw() {
+			return "Grade is too high";
+		}
+	};
+
+	class GradeTooLowException: public std::exception {
+	public:
+		virtual const char *what() const throw() {
+			return "Grade is too low";
+		}
+	};
+};
+
+std::ostream &operator<<(std::ostream &o, Bureaucrat const &rhs);
+
+#endif
---
+++ ex00/srcs/main.cpp
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: synicole <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/28 13:45:30 by synicole          #+#    #+#             */
+/*   Updated: 2023/06/28 13:45:32 by synicole         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+#include "Bureaucrat.class.hpp"
+
+int main(void) {
+
+	std::cout << std::endl << "--- CORRECT TESTS ---" << std::endl;
+
+	std::cout << "Creating a Bureaucrat with grade 150" << std::endl;
+	try {
+		Bureaucrat bureaucrat("Bernard", 150);
+		std::cout << bureaucrat;
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << std::endl << "Creating a Bureaucrat with grade 1" << std::endl;
+	try {
+		Bureaucrat bureaucrat("Bernard", 1);
+		std::cout << bureaucrat;
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << std::endl << "Creating a Bureaucrat with grade 2 and incrementing grade" << std::endl;
+	try {
+		Bureaucrat bureaucrat("Bernard", 2);
+		std::cout << bureaucrat;
+		bureaucrat.incrementGrade();
+		std::cout << bureaucrat;
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << std::endl << "Creating a Bureaucrat with grade 149 and decrementing grade" << std::endl;
+	try {
+		Bureaucrat bureaucrat("Bernard", 149);
+		std::cout << bureaucrat;
+		bureaucrat.decrementGrade();
+		std::cout << bureaucrat;
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << std::endl << "--- WRONG TESTS ---" << std::endl;
+
+	std::cout << std::endl << "Creating a Bureaucrat with grade 151" << std::endl;
+	try {
+		Bureaucrat bureaucrat("Bernard", 151);
+		std::cout << bureaucrat;
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << std::endl << "Creating a Bureaucrat with grade 0" << std::endl;
+	try {
+		Bureaucrat bureaucrat("Bernard", 0);
+		std::cout << bureaucrat;
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << std::endl << "Creating a Bureaucrat with grade 1 and incrementing grade" << std::endl;
+	try {
+		Bureaucrat bureaucrat("Bernard", 1);
+		std::cout << bureaucrat;
+		bureaucrat.incrementGrade();
+		std::cout << bureaucrat;
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << std::endl << "Creating a Bureaucrat with grade 150 and decrementing grade" << std::endl;
+	try {
+		Bureaucrat bureaucrat("Bernard", 150);
+		std::cout << bureaucrat;
+		bureaucrat.decrementGrade();
+		std::cout << bureaucrat;
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	return 0;
+}
